3d76a4f0dfa9eec5881c1a2552b797c7341e46ae
OWAIS KHAN <A0076323@kiongroup.com>
2025-05-24 01:16:20 -0400

    Added requests for user add and get by Id

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from 'express';
+import { CreateUserCommand } from '../models/CreateUserCommand';
 
 const router = Router();
 
@@ -10,5 +11,38 @@ router.post('/echo', (req: Request, res: Response) => {
     console.log(req.body.message);
     res.json({ received: req.body });
 });
+
+router.post('/users', (req: Request<{}, {}, CreateUserCommand>, res: Response): void => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400).json({ error: 'Name, email, and password are required' });
+    return;
+  }
+
+  const newUser = {
+    id: Date.now(), // example id generation
+    name,
+    email,
+  };
+
+  console.log(`Name: ${name}, Email: ${email}`)
+
+  res.status(201).json({
+    message: 'User created successfully',
+    user: newUser,
+  });
+});
+
+router.get('/users/:id', (req: Request<{id: string}, {}, {}, {}, {}>, res: Response) => {
+    const userId = req.params.id;
+    console.log(`UserId passed: ${userId}`);
+
+    res.status(201).json({
+        userId: userId,
+        user: "John Edward",
+        email: "john@gmail.com"
+    });
+})
 
 export default router;
